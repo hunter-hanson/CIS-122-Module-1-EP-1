9b1301f127a141eb26e00c89193b7b79b062a5c4
Hunter Hanson <122552678+hunter-hanson@users.noreply.github.com>
2024-12-13 13:32:04 -0600

    Add files via upload

---
+++ CIS 122 Module 1 EP 1.java
@@ -0,0 +1,30 @@
+class Main {
+  public static void main(String[] args) {
+    //1.11.1
+    System.out.println("Hey, fellow human!");
+    //1.11.2
+    int sum = 1+2+3+4+5+6+7+8+9+10;
+    System.out.println(sum);
+    //1.11.3
+    double product = 1*2*3*4*5*6*7*8*9*10;
+    System.out.println(product);
+    //1.11.4
+    double balance = 1000;
+    double rate = 0.05;
+    
+    double oneYear = balance + balance*rate;
+    System.out.println(oneYear);
+
+    double twoYears = oneYear + oneYear*rate;
+    System.out.println(twoYears);
+
+    double threeYears = twoYears +twoYears*rate;
+    System.out.println(threeYears);
+
+    //1.11.5
+    
+    
+      
+    
+  }
+}
